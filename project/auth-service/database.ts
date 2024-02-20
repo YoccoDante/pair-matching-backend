c1ceeb25e3e5fc5f4bc90ef043eb8b38d419cdf6
YoccoDante <hi.seresx@gmail.com>
2024-02-19 23:05:36 -0500

    adding multiplayer service, with bottles cooperative basics

--- project/auth-service/database.ts
+++ project/auth-service/database.ts
@@ -1,8 +1,58 @@
 import mongoose from 'mongoose';
-import { MONGO_DATA } from './mongo.config';
+import SecretsClient from "./src/clients/secrets";
+import CryptoService from "./src/services/crypto";
 
-export const connectToDatabase = () => {
-    mongoose.connect(MONGO_DATA.mongoURI, { useUnifiedTopology: true })
-        .then(() => console.log('MongoDB connected...'))
-        .catch(err => console.log(err));
+class Database {
+    private MONGO_URI_ENCRYPTED = {
+        iv: '76286b3546e255ea4f1e9f1784a48795',
+        content: '7963e6dbdf71e0260e2afd34a5914dbef0b7246372db43bb07457444da6d17b550718817860fc19bab05e15a7c047f2bd16d1302e7785e0d1db82a20d14f32fed36bdd072a5855b31362d73e4f9f092f9a7f31ab80d5f127e20c288c81838d1164499aae8e3fec10ea57e03bb3a0c419f5201dc7e48d058ed189db15ed5cbb28'
+    };
+
+    async getSecretKey(): Promise<string> {
+        try {
+            const client = SecretsClient.getInstance();
+            const secretKey = client.secret;
+            if (!secretKey) {
+                throw new Error(`Imposible obtener el secreto`);
+            }
+            return secretKey;
+        } catch (error) {
+            console.error(`Error al obtener la clave secreta: ${error}`);
+            throw error;
+        }
+    }
+    
+    async decryptMongoUri(secretKey: string): Promise<string> {
+        try {
+            const crypto = new CryptoService(secretKey);
+            const value = crypto.decrypt(this.MONGO_URI_ENCRYPTED);
+            return value;
+        } catch (error) {
+            console.error(`Error al desencriptar el URI de MongoDB: ${error}`);
+            throw error;
+        }
+    }
+    
+    async getMongoUri(): Promise<string> {
+        try {
+            const secretKey = await this.getSecretKey();
+            const mongoUri = await this.decryptMongoUri(secretKey);
+            return mongoUri;
+        } catch (error) {
+            console.error(`Error al obtener el URI de MongoDB: ${error}`);
+            throw error;
+        }
+    }
+
+    async connect() {
+        try {
+            const uri = await this.getMongoUri();
+            await mongoose.connect(uri);
+            console.log('MongoDB connected...');
+        } catch (error:any) {
+            throw new Error(`Imposible conectar a la base de datos ${error.message}`);
+        }
+    }
 }
+
+export default Database;
--- project/auth-service/server.ts
+++ project/auth-service/server.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { connectToDatabase } from './database';
+import Database from './database';
 import { userRouter } from './src/routes/user';
 import { authRouter } from './src/routes/auth';
+import SecretsClient from './src/clients/secrets';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Conectar a la base de datos
-connectToDatabase();
+// Inicializar SecretsClient
+const secretsClient = SecretsClient.getInstance();
+secretsClient.getSecretKey().then(() => {
+    // Conectar a la base de datos
+    const database = new Database();
+    database.connect().then(() => {
+        // Iniciar el servidor
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    }).catch((error) => {
+        throw new Error(`${error.message}`)
+    });
+    console.log('Secret key obtained');
+}).catch((error) => {
+    console.error(`Error obtaining secret key: ${error}`);
+});
 
 // Middleware para parsear el cuerpo de las solicitudes
 app.use(bodyParser.json());
@@ -16,8 +32,3 @@ app.use(bodyParser.json());
 // Configurar rutas
 app.use('/user', userRouter);
 app.use('/auth', authRouter);
-
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
